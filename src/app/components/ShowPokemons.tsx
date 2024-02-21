8e3e0708d4ff100439b9d542606181f496577a04
tecnologia-gaslive <pedro.edu2167@gmail.com>
2024-02-21 14:01:48 -0300

    fix Style

--- src/app/components/ShowPokemons.tsx
+++ src/app/components/ShowPokemons.tsx
@@ -14,24 +14,34 @@ export default function ShowPokemons({ pokemons }: PokemonProps) {
     <>
       <div className="max-w-[1300px] mx-auto">
         <div className="mt-9">
-          <h1 className="text-center text-2xl font-semibold">List Of 999 Pokemons:</h1>
+          <h1 className="text-center text-2xl font-semibold">
+            List Of 999 Pokemons:
+          </h1>
         </div>
         <div>
           <ul className="flex flex-wrap w-full justify-around mt-5">
             {allPokemons &&
               allPokemons.map((pokemon, index) => (
-                <div className="flex flex-col items-center  w-1/4 mx-3 my-3">
-                  <Image
-                    src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${(
-                      "000" +
-                      (index + 1)
-                    ).slice(-3)}.png`}
-                    alt={`Pokemon number: ${index}`}
-                    width={180}
-                    height={180}
-                  />
-                  <li className="p-8 font-medium text-xl" id={`number: ${index}`} key={index}>
-                    {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                <div className="flex flex-col items-center w-2/12 mx-3 my-3 bg-[#f2f2f2]">
+                  <div className="bg-[#a5a5a5] mt-3">
+                    <Image
+                      src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${(
+                        "000" +
+                        (index + 1)
+                      ).slice(-3)}.png`}
+                      alt={`Pokemon number: ${index}`}
+                      width={180}
+                      height={180}
+                      priority={false}
+                    />
+                  </div>
+                  <li
+                    className="p-8 font-medium text-xl text-[#313131]"
+                    id={`number: ${index}`}
+                    key={index}
+                  >
+                    {pokemon.name.charAt(0).toUpperCase() +
+                      pokemon.name.slice(1)}
                   </li>
                 </div>
               ))}
