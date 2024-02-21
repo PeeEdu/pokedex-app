8e6fe7f9a3d9c3fe986d0ed1705a737e75b86453
tecnologia-gaslive <pedro.edu2167@gmail.com>
2024-02-21 13:45:39 -0300

    UpperCase first letter

--- src/app/components/ShowPokemons.tsx
+++ src/app/components/ShowPokemons.tsx
@@ -2,30 +2,42 @@ import React from "react";
 import Image from "next/image";
 
 type PokemonProps = {
-    pokemons?: {
-        results: { name: string }[]
-    }
-}
+  pokemons?: {
+    results: { name: string }[];
+  };
+};
 
-export default function ShowPokemons({ pokemons }: PokemonProps){
-    const allPokemons = pokemons?.results;
+export default function ShowPokemons({ pokemons }: PokemonProps) {
+  const allPokemons = pokemons?.results;
 
-    
-
-    return(
-        <>
-            <div>
-                <h1>Pokemons:</h1>
-                <ul className="flex flex-wrap w-full">
-                
-                    {allPokemons && allPokemons.map((pokemon, index) => (
-                        <div className="flex flex-col items-center">
-                            <Image src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${('000' + (index + 1) ).slice(-3)}.png`} alt={`Pokemon number: ${index}`} width={70} height={70}/>
-                            <li className="p-8" id={`number: ${index}`} key={index}>{pokemon.name}</li>
-                        </div>
-                    ))}
-                </ul>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="max-w-[1300px] mx-auto">
+        <div className="mt-9">
+          <h1 className="text-center text-2xl font-semibold">List Of 999 Pokemons:</h1>
+        </div>
+        <div>
+          <ul className="flex flex-wrap w-full justify-around mt-5">
+            {allPokemons &&
+              allPokemons.map((pokemon, index) => (
+                <div className="flex flex-col items-center  w-1/4 mx-3 my-3">
+                  <Image
+                    src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${(
+                      "000" +
+                      (index + 1)
+                    ).slice(-3)}.png`}
+                    alt={`Pokemon number: ${index}`}
+                    width={180}
+                    height={180}
+                  />
+                  <li className="p-8 font-medium text-xl" id={`number: ${index}`} key={index}>
+                    {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                  </li>
+                </div>
+              ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
